bb0e779b8107a8361ad2d5bcd846fd5162300d43
Abdullah <abdullahalmasri@Abdullahs-MacBook-Air-5.local>
2020-05-25 13:24:49 -0500

    changes to code

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,7 +1,7 @@
 // @TODO: YOUR CODE HERE!
 
 var svgWidth = 960;
-var svgHeight = 660;
+var svgHeight = 600;
 
 var chartMargin = {
     top: 30,
@@ -15,7 +15,7 @@ var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
 var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
 
 var svg = d3
-    .select(".scatter")
+    .select("#scatter")
     .append("svg")
     .classed("svg_scatter",true)
     .attr("height", svgHeight)
@@ -23,24 +23,44 @@ var svg = d3
 
 var chartGroup = svg.append("g")
     .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
-  
 
 d3.csv("./assets/data/data.csv").then(function(Data) {
 
     console.log(Data);
 
-    // var smokes = Data.map(data => data.smokes);
-    // var age = Data.map(data => data.age);
-    // var state = Data.map(data =>data.state);
+    var x = d3.scaleLinear()
+        .domain([28, d3.max(Data, data => data.age) ])
+        .range([ 0, chartWidth ]);
+        
+    svg.append("g")
+        .attr("transform", "translate(0," + chartHeight + ")")
+        .call(d3.axisBottom(x));
 
-    // console.log(state);
-    // console.log(smokes);
-    // console.log(age);
+
+    var y = d3.scaleLinear()
+        .domain([9,25])
+        .range([ chartHeight, 0 ]);
+        
+    svg.append("g")
+        .attr("transform", "translate(30,0)")
+        .call(d3.axisLeft(y));
+
 
     Data.forEach(function(data) { 
         data.smokes = +data.smokes;
         data.age = +data.age;
     })
+
+    svg.append('g')
+        .selectAll("dot")
+        .data(Data)
+        .enter()
+        .append("circle")
+            .attr("cx", function (data) { return x(data.age); } )
+            .attr("cy", function (data) { return y(data.smokes); } )
+            .attr("r", 5)
+            .style("fill", "#69b3a2") 
+
 
 });
   
